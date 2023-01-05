f0c610420cd4559bae2a67da3e2a43006ec305b3
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2023-01-05 06:36:38 -0300

    Atualizar

---
+++ Aula Condições/index.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60) {
+    console.log(`Voçe ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
