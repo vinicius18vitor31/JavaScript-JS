9ae1659c50720d62b14a51ba626ce2e1e6948c49
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2023-01-09 19:23:49 -0300

    Nova Atualização

--- Exercicio04/script.js
+++ Exercicio04/script.js
@@ -1,33 +1,20 @@
-function contar() {
-    var ini = document.querySelector(`input#txti`)
-    var fim = document.querySelector(`input#txtf`)
-    var passo = document.querySelector(`input#txtp`)
-    var res = document.querySelector(`div#res`)
+function tabuada() {
+    var num = document.querySelector(`input#txtn`)
+    var tab = document.querySelector(`select#seltab`)
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert(`[ERRO] Faltam Dados!`)
-        res.innerHTML = `Impossível Contar!!`
-    } else { 
-        res.innerHTML = `Contando: ` 
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var passo = Number(passo.value)
-        if (passo <= 0) {
-            window.alert(`Passo Invalido! Considerando Passo 1 `)
-            p = 1
+    if (num.value.length == 0) {      
+        window.alert(`Por Favor Digite Um Número!`)
+    } else{
+        var n = Number(num.value)
+        var c = 1
+        while (c <= 10) {
+            var item = document.createElement(`option`)
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
-        if (i < f) {
-            for(var c = i; c <= f; c = c + passo) {
-                res.innerHTML += ` ${c} \u{1F449} `
-            }
-            res.innerHTML += `\u{1f3c1}`
-        } else {
-            for(var c = i; c >= f; c = c - passo) {
-                res.innerHTML += ` ${c} \u{1f449} `
-            }
-            res.innerHTML += `\u{1f3c1}`
-        }
-
-        
     }
+
+    
 }
