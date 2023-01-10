f10ad79bf3899101467e56735d62a2128387b8c4
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2023-01-10 07:41:03 -0300

    atualizando

---
+++ Variáveis Composta/index.js
@@ -0,0 +1,9 @@
+let num = [4, 5, 8, 9]
+
+num.push(1) //acrecenta um número ao ultimo elemento, neste caso o 1
+num.sort() //para colocar os números em ordem crescente
+console.log(`Nosso vetor é o ${num} `)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+
---
+++ Variáveis Composta/index01.js
@@ -0,0 +1,3 @@
+for(var pos=0; pos <num.length; pos = pos + pos) {
+    console.log(num[pos])
+}
