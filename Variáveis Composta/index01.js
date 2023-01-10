10be0afd9a49b9301aa657da4e205152d0768970
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2023-01-10 07:57:11 -0300

    Atualizando

--- Variáveis Composta/index01.js
+++ Variáveis Composta/index01.js
@@ -1,3 +1,5 @@
-for(var pos=0; pos <num.length; pos = pos + pos) {
-    console.log(num[pos])
+var valores = [5, 8, 9, 7, 6] 
+
+for(var pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
