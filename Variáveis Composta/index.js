a816613998f5249ac360bca2a83bc187fc56cc21
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2023-01-10 21:21:26 -0300

    Atualizando

--- Variáveis Composta/index.js
+++ Variáveis Composta/index.js
@@ -6,4 +6,10 @@ console.log(`Nosso vetor é o ${num} `)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
+let pos = num.indexOf(10)
+if(pos == -1){
+    console.log(`O valor não foi encontrado`)
+} else{
+    console.log(`O valor 8 está na posição ${pos}`)
+}
 
---
+++ Variáveis Composta/index02.js
@@ -0,0 +1,5 @@
+var valores = [5, 8, 9, 7, 6] 
+
+for(var pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
