a99d9528f635d6e77c9ee6b5cd8aa66e3fb27526
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2023-01-18 21:32:10 -0300

    atualizando

---
+++ VariaveisOrigamid/script.js
@@ -0,0 +1,37 @@
+var nome = 'Vinicius';
+var idade= 37;
+var possuiFaculdade = 'true';
+
+var sobreNome = 'Souza', 
+    cidade = 'Caldas do Jorro';
+console.log(sobreNome, cidade)
+
+var time = 'Bahia'
+var time = 'Vitoria'
+console.log(time)
+/*Usando o var é possivel criar duas variaveis com o mesmo nome e o console.log() vai ler a ultima declaração
+*/
+let clube = 'Vitoria';
+//let clube = 'Bahia';
+console.log(clube)
+/*
+Usando a variavel let não é possivel criar duas variaveis com o mesmo nome pois já acusará erro;
+*/
+
+//Atividade
+// Declarar uma variável com o seu nome
+var nome = 'Vinicius';
+// Declarar uma variável com a sua idade
+var idade = 37;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'churasco';
+// Declarar 5 variáveis diferentes sem valores
+var casa; 
+    carro;
+    viagem;
+    time;
+    rua;
+console.log(nome, idade, comidaFavorita)
