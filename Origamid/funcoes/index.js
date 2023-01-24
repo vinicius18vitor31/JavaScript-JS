3827fd0a9826b2511f5b2a4d2919904ff8df2f4c
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2023-01-24 19:11:54 -0300

    Nova atualização

--- Origamid/funcoes/index.js
+++ Origamid/funcoes/index.js
@@ -1,28 +1,49 @@
 // Crie uma função para verificar se um valor é Truthy
-
+function isTruthy(dado){
+    return !!dado;
+}
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
 
 // Crie uma função que retorne o seu nome completo
 // ela deve possuir os parâmetros: nome e sobrenome
-
+function nomeCompleto(nome, sobreNome){
+    nome = 'Vinicius';
+    sobreNome = 'Souza';
+    return `Meu nome é ${nome} ${sobreNome}!`
+}
 // Crie uma função que verifica se um número é par
-
+function ePar(numero) {
+    if(numero % 2 === 0){
+        return 'Esse número é par!'
+    }else{
+        return 'Esse número é impar!'
+    }
+}
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
-
+function tipo(dado){
+    return typeof dado;
+}
+console.log(tipo(25))
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
-// quando o evento 'scroll' ocorrer.
-
+// quando o evento 'click' ocorrer.
+addEventListener('click', function(){
+    console.log('Vinicius Souza')
+});
 // Corrija o erro abaixo
+var totalPaises = 193;
+
 function precisoVisitar(paisesVisitados) {
-    var totalPaises = 193;
     return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
-  }
-  function jaVisitei(paisesVisitados) {
+}
+function jaVisitei(paisesVisitados) {
     return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
-  }
-  precisoVisitar(20);
-  jaVisitei(20);
+}
+precisoVisitar(20);
+jaVisitei(20);
+console.log(jaVisitei(20))
+  
+  
--- Origamid/funcoes/teste.js
+++ Origamid/funcoes/teste.js
@@ -1,4 +1,45 @@
-// Crie uma função para verificar se um valor é Truthy
-function veri(){
-    
+function isTruthy(dado){
+    return !!dado;
 }
+console.log(isTruthy(25))
+
+function perimetroQuadrado(lado){
+    return lado * 4;
+}
+console.log(perimetroQuadrado(8))
+
+function nomeCompleto(nome, sobreNome){
+    nome = 'Vinicius';
+    sobreNome = 'Souza';
+    return `Meu nome é ${nome} ${sobreNome}!`
+}
+console.log(nomeCompleto())
+
+function ePar(numero) {
+    if(numero % 2 === 0){
+        return 'Esse número é par!'
+    }else{
+        return 'Esse número é impar!'
+    }
+}
+console.log(ePar(19))
+
+function tipo(dado){
+    return typeof dado;
+}
+console.log(tipo(25))
+/*
+addEventListener('click', function(){
+    console.log('Vinicius Souza')
+});
+*/
+var totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
+    return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+    return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+
+console.log(jaVisitei(20))
