41bb255f1984df1ef39e3e6359a6dc16ac7c78bc
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2023-04-21 07:28:50 -0300

    Atualização

--- Origamid/arreyELoops/teste.js
+++ Origamid/arreyELoops/teste.js
@@ -4,3 +4,8 @@ console.log(videoGame);
 for (let numero = 0; numero < 15; numero++) {
     console.log(numero);
 };
+
+const Game = ['Nitendo', 'PS2', 'NeoGeo', 'PS4'];
+for(let item = 0; item < Game.length; item++){
+    console.log(Game[item]);
+};
