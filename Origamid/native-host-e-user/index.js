8d88712de07bb84c821dac96829708e1b65b6dd9
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2023-05-18 11:35:21 -0300

    Atualizando

---
+++ Origamid/native-host-e-user/index.js
@@ -0,0 +1,17 @@
+// Liste 5 objetos nativos
+Object
+String
+Array
+Number
+math
+
+// Liste 5 objetos do browser
+window
+document
+NodeList
+HTMLCollection
+history
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
